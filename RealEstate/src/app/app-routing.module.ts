cd722dc9466b868f09fadf8afc5caadc2df0c76c
Tôn Võ Thủy Tiên <34209563+TonVoThuyTien@users.noreply.github.com>
2020-06-04 16:10:09 +0700

    Home

--- RealEstate/src/app/app-routing.module.ts
+++ RealEstate/src/app/app-routing.module.ts
@@ -7,21 +7,6 @@ import { HomeComponent } from './Home/Home.component';
 
 const routes: Routes = [{ path: '', redirectTo: 'Home', pathMatch: 'full' },
 { path: 'Masterpage', component: MasterpageComponent },
-// {
-//   path: 'Main',
-//   component: MainComponent,
-//   children: [
-//     {
-//       path: 'Registration',
-//       component: RegistrationComponent
-//     },
-//     {
-//       path: 'Login',
-//       component: LoginComponent
-//     }
-//   ]
-// },
-// { path: 'Detail', component: DetailComponent },
 { path: 'Home', component: HomeComponent },
 ];
 
